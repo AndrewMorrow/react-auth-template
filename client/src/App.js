f4ae0209a9a2cb5f33fe4bec831a51ed7bcf1e30
Andrew Morrow <beardedmongo@gmail.com>
2021-05-23 21:25:45 -0500

    starts register page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // Components
 import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
 import Landing from "./components/Landing/Landing";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <Router>
             <Route exact path="/" component={Landing} />
             <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
         </Router>
     );
 }
