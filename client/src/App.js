a6f4252957592e8256892328e76e4c4b14975dd9
AndrewMorrow <beardedmongo@gmail.com>
2021-05-23 20:28:02 -0500

    adds routes for register and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// Components
+import Register from "./components/Register/Register";
+import Landing from "./components/Landing/Landing";
 
 function App() {
-    return <div className="App">Hello</div>;
+    return (
+        <Router>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+        </Router>
+    );
 }
 
 export default App;
---
+++ client/src/components/Landing/Landing.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+    return (
+        <main>
+            Home Page
+            <Link to="/register">Register </Link>
+        </main>
+    );
+};
+
+export default Landing;
