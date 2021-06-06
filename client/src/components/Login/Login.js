b462c7441c2e00a9eb628588ddca51d90164df66
Andrew Morrow <beardedmongo@gmail.com>
2021-06-05 23:04:41 -0500

    updates sign in and register page

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -3,76 +3,81 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
     return (
-        <div className="flex items-center min-h-screen bg-white dark:bg-gray-900">
-            <div className="container mx-auto">
-                <div className="max-w-md mx-auto my-10">
-                    <div className="text-center">
-                        <h1 className="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">
-                            Sign in
-                        </h1>
-                        <p className="text-gray-500 dark:text-gray-400">
-                            Sign in to access your account
-                        </p>
-                    </div>
-                    <div className="m-7">
-                        <form action="">
-                            <div className="mb-6">
-                                <label
-                                    for="email"
-                                    className="block mb-2 text-sm text-gray-600 dark:text-gray-400"
-                                >
-                                    Email Address
-                                </label>
-                                <input
-                                    type="email"
-                                    name="email"
-                                    id="email"
-                                    placeholder="you@company.com"
-                                    className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
-                                />
-                            </div>
-                            <div className="mb-6">
-                                <div className="flex justify-between mb-2">
-                                    <label
-                                        for="password"
-                                        className="text-sm text-gray-600 dark:text-gray-400"
-                                    >
-                                        Password
-                                    </label>
-                                    <a
-                                        href="#!"
-                                        className="text-sm text-gray-400 focus:outline-none focus:text-indigo-500 hover:text-indigo-500 dark:hover:text-indigo-300"
-                                    >
-                                        Forgot password?
-                                    </a>
-                                </div>
-                                <input
-                                    type="password"
-                                    name="password"
-                                    id="password"
-                                    placeholder="Your Password"
-                                    className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
-                                />
-                            </div>
-                            <div className="mb-6">
-                                <button
-                                    type="button"
-                                    className="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none"
-                                >
-                                    Sign in
-                                </button>
-                            </div>
-                            <p className="text-sm text-center text-gray-400">
-                                Don&#x27;t have an account yet?{" "}
-                                <Link
-                                    to="/register"
-                                    className="text-indigo-400 focus:outline-none focus:underline focus:text-indigo-500 dark:focus:border-indigo-800"
-                                >
-                                    Sign up
-                                </Link>
-                                .
+        <div className=" min-h-screen bg-white dark:bg-gray-200 flex items-center justify-center">
+            <div
+                className="bg-gray-100 text-gray-500 rounded-3xl shadow-xl w-full overflow-hidden mx-10"
+                style={{ "max-width": "500px" }}
+            >
+                <div className="container mx-auto">
+                    <div className="max-w-md mx-auto my-10">
+                        <div className="text-center">
+                            <h1 className="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-900">
+                                Sign in
+                            </h1>
+                            <p className="text-gray-500 dark:text-gray-400">
+                                Sign in to access your account
                             </p>
-                        </form>
+                        </div>
+                        <div className="m-7">
+                            <form action="">
+                                <div className="mb-6">
+                                    <label
+                                        for="email"
+                                        className="block mb-2 text-sm text-gray-600 dark:text-gray-400"
+                                    >
+                                        Email Address
+                                    </label>
+                                    <input
+                                        type="email"
+                                        name="email"
+                                        id="email"
+                                        placeholder="you@company.com"
+                                        className="w-full px-3 py-2 placeholder-gray-300 border-2 border-gray-200 rounded-lg outline-none focus:border-indigo-300 dark:bg-gray-100 dark:text-white dark:placeholder-gray-500 dark:border-gray-200 dark:focus:border-indigo-500"
+                                    />
+                                </div>
+                                <div className="mb-6">
+                                    <div className="flex justify-between mb-2">
+                                        <label
+                                            for="password"
+                                            className="text-sm text-gray-600 dark:text-gray-400"
+                                        >
+                                            Password
+                                        </label>
+                                        <a
+                                            href="#!"
+                                            className="text-sm text-gray-400 focus:outline-none focus:text-indigo-500 hover:text-indigo-500 dark:hover:text-indigo-300"
+                                        >
+                                            Forgot password?
+                                        </a>
+                                    </div>
+                                    <input
+                                        type="password"
+                                        name="password"
+                                        id="password"
+                                        placeholder="Your Password"
+                                        className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md outline-none focus:border-indigo-300 dark:bg-gray-100 dark:text-white dark:placeholder-gray-500 dark:border-gray-200 dark:focus:border-gray-500"
+                                    />
+                                </div>
+                                <div className="mb-6">
+                                    <button
+                                        type="button"
+                                        className="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none"
+                                    >
+                                        Sign in
+                                    </button>
+                                </div>
+                                <p className="text-sm text-center text-gray-400">
+                                    Don&#x27;t have an account yet?{" "}
+                                    <Link
+                                        to="/register"
+                                        className="text-indigo-400 focus:outline-none focus:underline focus:text-indigo-500 dark:focus:border-indigo-800"
+                                    >
+                                        Sign up
+                                    </Link>
+                                    .
+                                </p>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -4,58 +4,104 @@ import { Link } from "react-router-dom";
 const Register = () => {
     return (
         <main>
-            <div className="flex items-center min-h-screen bg-white dark:bg-primary-dark">
-                <div className="container mx-auto ">
-                    <Link to="/login" className="text-primary-light">
-                        Login
-                    </Link>
-                    <div className="max-w-md mx-auto my-10">
-                        <div className="text-center">
-                            <h1 className="font-semibold text-3xl my-3 text-primary-light">
-                                Register Here
-                            </h1>
-                            <p className="text-secondary">
-                                Sign up for a new account here
-                            </p>
-                        </div>
-
-                        <div className="flex m-4 justify-between">
-                            <div className=" ">
-                                <form action="">
-                                    <div>
-                                        <label
-                                            for="firstName"
-                                            className="text-secondary-light"
-                                        >
-                                            First Name
-                                        </label>
-                                        <input
-                                            type="input"
-                                            name="firstName"
-                                            placeholder="First Name"
-                                            className="w-full py-2 px-3"
-                                        />
-                                    </div>
-                                </form>
+            <div className="min-w-screen min-h-screen bg-gray-900 flex items-center justify-center px-5 py-5">
+                <div
+                    className="bg-gray-100 text-gray-500 rounded-3xl shadow-xl w-full overflow-hidden mx-10"
+                    style={{ "max-width": "1000px" }}
+                >
+                    <div className="md:flex w-full">
+                        <div className="w-full  py-10 px-5 md:px-10">
+                            <div className="text-center mb-10">
+                                <h1 className="font-bold text-3xl text-gray-900">
+                                    REGISTER
+                                </h1>
+                                <p>Enter your information to register</p>
                             </div>
 
-                            <div className="">
-                                <form action="">
-                                    <div className="mb-6">
-                                        <label
-                                            for="lastName"
-                                            className="text-secondary-light"
-                                        >
-                                            Last Name
-                                        </label>
+                            <div className="flex -mx-3">
+                                <div className="w-1/2 px-3 mb-5">
+                                    <label
+                                        for=""
+                                        className="text-xs font-semibold px-1"
+                                    >
+                                        First name
+                                    </label>
+                                    <div className="flex">
+                                        <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"></div>
                                         <input
-                                            type="input"
-                                            name="firstName"
-                                            placeholder="Last Name"
-                                            className="w-full py-2 px-3"
+                                            type="text"
+                                            className="w-full -ml-10 pl-2 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                                            placeholder="John"
                                         />
                                     </div>
-                                </form>
+                                </div>
+                                <div className="w-1/2 px-3 mb-5">
+                                    <label
+                                        for=""
+                                        className="text-xs font-semibold px-1"
+                                    >
+                                        Last name
+                                    </label>
+                                    <div className="flex">
+                                        <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"></div>
+                                        <input
+                                            type="text"
+                                            className="w-full -ml-10 pl-2 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                                            placeholder="Smith"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex -mx-3">
+                                <div className="w-full px-3 mb-5">
+                                    <label
+                                        for=""
+                                        className="text-xs font-semibold px-1"
+                                    >
+                                        Email
+                                    </label>
+                                    <div className="flex">
+                                        <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"></div>
+                                        <input
+                                            type="email"
+                                            className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                                            placeholder="johnsmith@example.com"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex -mx-3">
+                                <div className="w-full px-3 mb-12">
+                                    <label
+                                        for=""
+                                        className="text-xs font-semibold px-1"
+                                    >
+                                        Password
+                                    </label>
+                                    <div className="flex">
+                                        <div className="w-10 z-10 pl-1 text-center pointer-events-none flex items-center justify-center"></div>
+                                        <input
+                                            type="password"
+                                            className="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+                                            placeholder="************"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex -mx-3">
+                                <div className="w-full px-3 mb-5">
+                                    <button className="block w-full max-w-xs mx-auto bg-indigo-500 hover:bg-indigo-700 focus:bg-indigo-700 text-white rounded-lg px-3 py-3 font-semibold">
+                                        REGISTER NOW
+                                    </button>
+                                </div>
+                            </div>
+                            <div className="text-center">
+                                <p>
+                                    Already have an account?{" "}
+                                    <a href="/login" className="text-blue-500">
+                                        Login Here
+                                    </a>
+                                </p>
                             </div>
                         </div>
                     </div>
