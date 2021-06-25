426a9140870def6c0d2736fb5f917140cf85e901
AndrewMorrow <beardedmongo@gmail.com>
2021-06-25 10:58:55 -0500

    adds user icon to navbar

--- client/src/components/partials/Navbar/Navbar.js
+++ client/src/components/partials/Navbar/Navbar.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { FaUserCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const openMenu = () => {
+    console.log("open");
+  };
+
   return (
     <header>
       <nav className="bg-blue-300 py-4 mb-6">
         <div className="container flex justify-between max-w-5xl xl:max-w-7xl">
           <div className="">Branding</div>
-          <div className="">Items</div>
+          <div className="flex items-center space-x-6 md:space-x-10">
+            <Link to="#">Link</Link>
+            <Link to="#">Link</Link>
+            <FaUserCircle onClick={openMenu} size="1.5rem" />
+          </div>
         </div>
       </nav>
     </header>
